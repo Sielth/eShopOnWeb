913c5b917deeb8c60188d9f4b615a98927cb4deb
Sielth <sielth@hotmail.it>
2023-03-07 10:07:01 +0100

    deliveryfee added and tested

--- src/Web/Pages/Basket/BasketViewModel.cs
+++ src/Web/Pages/Basket/BasketViewModel.cs
@@ -5,9 +5,14 @@ public class BasketViewModel
     public int Id { get; set; }
     public List<BasketItemViewModel> Items { get; set; } = new List<BasketItemViewModel>();
     public string? BuyerId { get; set; }
+    public decimal Delivery { get; set; } = 300m;
 
     public decimal Total()
     {
-        return Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
+        var total = Math.Round(Items.Sum(x => x.UnitPrice * x.Quantity), 2);
+
+        if (total > Delivery) Delivery = 0;
+        
+        return total += Delivery;
     }
 }
---
+++ tests/UnitTests/Web/Pages/BasketViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.eShopWeb.Web.Pages.Basket;
+using Xunit;
+
+namespace Microsoft.eShopWeb.UnitTests.Web.Pages;
+
+public class BasketViewModelTests
+{
+    private BasketViewModel? _basketViewModel;
+
+    [Fact]
+    public void DeliveryFee()
+    {
+        // Arrange
+        _basketViewModel = new BasketViewModel
+        {
+            Id = 1,
+            BuyerId = "buyer1",
+            Items = new List<BasketItemViewModel>
+            {
+                new()
+                {
+                    CatalogItemId = 1,
+                    Id = 1,
+                    OldUnitPrice = 10,
+                    PictureUrl = "string",
+                    ProductName = "string",
+                    Quantity = 10,
+                    UnitPrice = 10m
+                }
+            }
+        };
+        
+        var expectedDelivery = 300m;
+
+        // Act
+        _basketViewModel.Total();
+
+        // Assert
+        Assert.Equal(expectedDelivery, _basketViewModel.Delivery);
+    }
+}
