2b2e8cd86d95b4fa90000c0986610e9fde2ecb78
TomasRJ <49104467+TomasRJ@users.noreply.github.com>
2023-03-09 10:05:18 +0100

    begyndelse af FavouriteQueryService og test

---
+++ src/Infrastructure/Data/Queries/FavouriteQueryService.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+
+namespace Microsoft.eShopWeb.Infrastructure.Data.Queries;
+public class FavouriteQueryService : IFavouriteQueryService
+{
+    private readonly CatalogContext _dbContext;
+    public FavouriteQueryService(CatalogContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<int> CountTotalFavourites(string username)
+    {
+        // TODO
+        return 0;
+    }
+}
---
+++ tests/UnitTests/Infrastructure/Queries/CountTotalFavouritesTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Services;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Moq;
+using Xunit;
+using Microsoft.eShopWeb.Infrastructure.Data;
+
+namespace Microsoft.eShopWeb.UnitTests.Infrastructure.Queries;
+public class CountTotalFavouritesTest
+{
+    private readonly string _buyerId = "buyer1";
+    //private readonly Mock<CatalogContext> _mockdbContext = new();
+}
