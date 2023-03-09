0bd85a79c142a4642d63f9af04dffcbc98594c30
TomasRJ <49104467+TomasRJ@users.noreply.github.com>
2023-03-09 12:04:11 +0100

    Lavede count til FavouritesViewModelService

--- src/Web/Services/FavouritesViewModelService.cs
+++ src/Web/Services/FavouritesViewModelService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities.FavouriteAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.Web.Interfaces;
 using Microsoft.eShopWeb.Web.Pages.Favourites;
 
@@ -6,9 +7,21 @@ namespace Microsoft.eShopWeb.Web.Services;
 
 public class FavouritesViewModelService : IFavouritesViewModelService
 {
+    private readonly IFavouriteQueryService _favouriteQueryService;
+
+    public FavouritesViewModelService(IFavouriteQueryService favouriteQueryService)
+    {
+        _favouriteQueryService = favouriteQueryService;
+    }
     public Task<FavouritesViewModel> Map(Favourite favourite, CancellationToken cancellationToken)
     {
         return null;
         // TODO: implement
+    }
+    public async Task<int> CountTotalFavourites(string username)
+    {
+        var counter = await _favouriteQueryService.CountTotalFavourites(username);
+
+        return counter;
     }
 }
